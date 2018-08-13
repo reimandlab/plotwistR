d1cd608bf13ffe846ceea978569c48d6f6761992
karini925 <karin.isaev@gmail.com>
2018-08-13 10:27:21 -0400

    Update tileplotRL.R

--- Draft_Functions/tileplotRL.R
+++ Draft_Functions/tileplotRL.R
@@ -1,1 +1,21 @@
+# Set-UP ------------------------------------------------------------------
+require(RColorBrewer)
+require(ggplot2)
+data("esoph")
 
+# tileplotRL -----------------------------------------------------------
+tileplotRL = function(){
+  list(
+    geom_tile(color="grey"),
+    theme_bw(),
+    theme(axis.text.x = element_text(size = 10, face = 'plain', angle = 90, hjust = 0),
+          axis.text.y = element_text(size = 10, face = 'plain'),
+          axis.title = element_text(size = 20, face = 'plain'),
+          plot.title = element_text(size = 20, face = 'plain')),
+          scale_fill_gradient(low = "blue", high = "orange")
+  )
+}
+
+# Testing -----------------------------------------------------------------
+ggplot(esoph, aes(x=agegp, y=alcgp, fill = ncases)) + 
+  tileplotRL()
